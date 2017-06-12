219f02a5e670977aa606c3e7ba302eb0fffadaf9
Fabio Vergani <fabio.vergani.1975@gmail.com>
2017-06-12 15:39:04 +0200

    Create getStyleObject.js

---
+++ getStyleObject.js
@@ -0,0 +1,7 @@
+function getStyleObject(a,b=null){//elem,pseudo
+ const o={};
+ for(let s,p=/\-([a-z])/g,m=window.getComputedStyle(a,b),l=m.length,i=0;i<l;++i){s=m[i];o[s.replace(p,function(a,b){return b.toUpperCase()})]=m.getPropertyValue(s)};
+ return o;
+}
+
+//console.dir(getStyleObject(document.body))
